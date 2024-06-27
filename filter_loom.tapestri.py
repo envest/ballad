0818ebe333ca0535dfb98d5f0fc05e4dde946b8d
Steven Foltz (@envest) <stevenmasonfoltz@gmail.com>
2024-06-27 11:26:02 -0400

    Use dictionaries to progressively filter out indexes; output a filtered loom file

--- filter_loom.tapestri.py
+++ filter_loom.tapestri.py
@@ -1,5 +1,4 @@
 import sys
-import shutil
 import loompy
 import numpy as np
 
@@ -18,47 +17,60 @@ var_cb_file = output_dir + "/" + output_file_prefix + ".variant_cell_barcode.tsv
 var_sum_file = output_dir + "/" + output_file_prefix + ".variant_summary.tsv"
 filtered_loom_file = output_dir + "/" + output_file_prefix + ".loom"
 
-# copy existing loom file to filtered loom file
-shutil.copy2(loom_filename, filtered_loom_file)
-ds = loompy.connect(filtered_loom_file)
-ds_GT_copy = ds[:,:]
+# set up dictionaries to modify/filter
+layers_dict = {}
+row_dict = {}
+col_dict = {}
 
-#keep_var_index = []
-#keep_cb_index = []
-
-#n_variants = ds.shape[0]
-#n_cells = ds.shape[1]
+# connect to pre-filtered loom file
+with loompy.connect(loom_filename) as ds:
+  for k,v in ds.layers.items():
+    layers_dict[k] = v[:,:]
+  for k,v in ds.ra.items():
+    row_dict[k] = v
+  for k,v in ds.ca.items():
+    col_dict[k] = v
 
 # Filters 1-3: set genotype to missing if GQ < gq_min, DP < dp_min, or scVAF < scVAF_min
-# applied simultaneously because each genotype is independent
+# can be applied simultaneously because each genotype is independent
 
 def calculate_scVAF(GT, AD, DP):
     scVAF = np.array([[1.0]*GT.shape[1]]*GT.shape[0])
-    where_GT_is_12 = np.where(np.logical_and(list(GT[:,:] == 1) or list(GT[:,:] == 2), list(DP[:,:] > 0)))
+    where_GT_is_12 = np.where(np.logical_and(list(GT == 1) or list(GT == 2), list(DP > 0)))
     i = where_GT_is_12[0]
     j = where_GT_is_12[1]
-    scVAF[i,j] = np.divide(AD[:,:][i,j], DP[:,:][i,j])
+    scVAF[i,j] = np.divide(AD[i,j], DP[i,j])
     return(scVAF)
 
-scVAF = calculate_scVAF(ds, ds["AD"], ds["DP"])
+scVAF = calculate_scVAF(layers_dict[""], layers_dict["AD"], layers_dict["DP"])
 
-set_missing_index_array = np.where((ds["GQ"][:,:] < gq_min) | (ds["DP"][:,:] < dp_min) | (scVAF[:,:] < scVAF_min))
+set_missing_index_array = np.where((layers_dict["GQ"] < gq_min) | (layers_dict["DP"] < dp_min) | (scVAF < scVAF_min))
 
 i = set_missing_index_array[0]
 j = set_missing_index_array[1]
 
-ds_GT_copy[i,j] = 3
-ds[:,:] = ds_GT_copy
+# set genotypes to missing
+layers_dict[""][i,j] = 3
 
 # Filter 4: remove variants with genotype in < X% of cells
-prop_var_with_GT = 1 - np.apply_along_axis(np.mean, 1, ds[:,:] == 3)
+prop_var_with_GT = 1 - np.apply_along_axis(np.mean, 1, layers_dict[""] == 3)
 keep_var_index_F4 = np.where(prop_var_with_GT >= min_prop_var_with_GT)[0]
-ds = ds[keep_var_index_F4,:]
+
+for k,v in layers_dict.items():
+  layers_dict[k] = v[keep_var_index_F4,:]
+
+for k,v in row_dict.items():
+  row_dict[k] = v[keep_var_index_F4]
 
 # Filter 5: remove cells with genotype in < X% of variants
-prop_cell_with_GT = 1 - np.apply_along_axis(np.mean, 0, ds[:,:] == 3)
+prop_cell_with_GT = 1 - np.apply_along_axis(np.mean, 0, layers_dict[""] == 3)
 keep_cell_index_F5 = np.where(prop_cell_with_GT >= min_prop_cell_with_GT)[0]
-ds = ds[:,keep_cell_index_F5]
+
+for k,v in layers_dict.items():
+  layers_dict[k] = v[:,keep_cell_index_F5]
+
+for k,v in col_dict.items():
+  col_dict[k] = v[keep_cell_index_F5]
 
 # Filter 6: remove variants mutated in < X% of cells
 def prop_GT_mutated(n_GT_00, n_GT_not_missing):
@@ -67,16 +79,22 @@ def prop_GT_mutated(n_GT_00, n_GT_not_missing):
     prop_GT_00[has_GT] = n_GT_00[has_GT]/n_GT_not_missing[has_GT]
     return(1 - prop_GT_00)
 
-n_GT_00 = np.apply_along_axis(sum, 1, ds[:,:] == 0)
-n_GT_not_missing = np.apply_along_axis(sum, 1, ds[:,:] != 3)
+n_GT_00 = np.apply_along_axis(sum, 1, layers_dict[""] == 0)
+n_GT_not_missing = np.apply_along_axis(sum, 1, layers_dict[""] != 3)
 prop_var_mutated = prop_GT_mutated(n_GT_00, n_GT_not_missing)
 
 keep_var_index_F6 = np.where(prop_var_mutated >= min_prop_var_mutated)[0]
-ds = ds[keep_var_index_F6,:]
 
-# BY THIS POINT, DS is not a loom connection anymore
+for k,v in layers_dict.items():
+  layers_dict[k] = v[keep_var_index_F6,:]
 
-# write output files
+for k,v in row_dict.items():
+  row_dict[k] = v[keep_var_index_F6]
+
+# write filtered loom file
+loompy.create(filtered_loom_file, layers_dict, row_dict, col_dict)
+
+# write other output files
 
 var_cb = open(var_cb_file, "w")
 var_cb_header_list = ["chr", "pos", "ref", "alt", "cell_barcode", "GT", "DP", "AD_REF", "AD_ALT", "GQ"]
@@ -88,25 +106,25 @@ var_sum_header_list = ["chr", "pos", "ref", "alt", "n_cells", "n_00", "n_01", "n
 var_sum_header_line = "\t".join(var_sum_header_list) + "\n"
 var_sum.write(var_sum_header_line)
 
-n_var = ds.shape[0]
-n_cells = ds.shape[1]
+n_var = layers_dict[""].shape[0]
+n_cells = layers_dict[""].shape[1]
 
 for var_index in range(n_var):
-    chr = ds.ra["CHROM"][var_index]
-    pos = ds.ra["POS"][var_index]
-    ref = ds.ra["REF"][var_index]
-    alt = ds.ra["ALT"][var_index]
+    chr = row_dict["CHROM"][var_index]
+    pos = row_dict["POS"][var_index]
+    ref = row_dict["REF"][var_index]
+    alt = row_dict["ALT"][var_index]
     for cb_index in range(n_cells):
-        cb = ds.ca["barcode"][cb_index]
-        gt = ds_GT_copy[var_index, cb_index]
-        dp = ds["DP"][var_index, cb_index]
-        ad_ref = ds["RO"][var_index, cb_index]
-        ad_alt = ds["AD"][var_index, cb_index]
-        gq = ds["GQ"][var_index, cb_index]
+        cb = col_dict["barcode"][cb_index]
+        gt = layers_dict[""][var_index, cb_index]
+        dp = layers_dict["DP"][var_index, cb_index]
+        ad_ref = layers_dict["RO"][var_index, cb_index]
+        ad_alt = layers_dict["AD"][var_index, cb_index]
+        gq = layers_dict["GQ"][var_index, cb_index]
         var_cb_list = [str(x) for x in [chr, pos, ref, alt, cb, gt, dp, ad_ref, ad_alt, gq]]
         var_line = "\t".join(var_cb_list) + "\n"
         var_cb.write(var_line)
-    var = ds[var_index,:]
+    var = layers_dict[""][var_index,:]
     n_na = (var == 3).sum()
     n_not_na = n_cells - n_na
     n_00 = (var == 0).sum()
@@ -119,4 +137,3 @@ for var_index in range(n_var):
 
 var_cb.close()
 var_sum.close()
-ds.close()
