1ea1eabd9727c17314dd627a0283e3c7375ac3ec
Steven Foltz (@envest) <stevenmasonfoltz@gmail.com>
2024-06-26 20:07:55 -0400

    Ensure proper filtering and formatting

--- filter_loom.tapestri.py
+++ filter_loom.tapestri.py
@@ -21,41 +21,44 @@ filtered_loom_file = output_dir + "/" + output_file_prefix + ".loom"
 # copy existing loom file to filtered loom file
 shutil.copy2(loom_filename, filtered_loom_file)
 ds = loompy.connect(filtered_loom_file)
+ds_GT_copy = ds[:,:]
 
-keep_var_index = []
-keep_cb_index = []
+#keep_var_index = []
+#keep_cb_index = []
 
-n_variants = ds.shape[0]
-n_cells = ds.shape[1]
+#n_variants = ds.shape[0]
+#n_cells = ds.shape[1]
 
 # Filters 1-3: set genotype to missing if GQ < gq_min, DP < dp_min, or scVAF < scVAF_min
 # applied simultaneously because each genotype is independent
 
 def calculate_scVAF(GT, AD, DP):
-    scVAF = np.array([[None]*GT.shape[1]]*GT.shape[0])
-    where_GT_is_01 = np.where((GT[:,:] == 1 | GT[:,:] == 2) & (DP[:,:] > 0))
-    i = where_GT_is_01[0]
-    j = where_GT_is_01[1]
+    scVAF = np.array([[1.0]*GT.shape[1]]*GT.shape[0])
+    where_GT_is_12 = np.where(np.logical_and(list(GT[:,:] == 1) or list(GT[:,:] == 2), list(DP[:,:] > 0)))
+    i = where_GT_is_12[0]
+    j = where_GT_is_12[1]
     scVAF[i,j] = np.divide(AD[:,:][i,j], DP[:,:][i,j])
     return(scVAF)
 
 scVAF = calculate_scVAF(ds, ds["AD"], ds["DP"])
 
-set_missing_index_array = np.where((ds["GQ"][:,:] < gq_min) | (ds["DP"][:,:] < dp_min) | (scVAF[:,:] is not None & scVAF[:,:] < scVAF_min))
+set_missing_index_array = np.where((ds["GQ"][:,:] < gq_min) | (ds["DP"][:,:] < dp_min) | (scVAF[:,:] < scVAF_min))
+
 i = set_missing_index_array[0]
 j = set_missing_index_array[1]
 
-ds[i,j] = 3
+ds_GT_copy[i,j] = 3
+ds[:,:] = ds_GT_copy
 
 # Filter 4: remove variants with genotype in < X% of cells
 prop_var_with_GT = 1 - np.apply_along_axis(np.mean, 1, ds[:,:] == 3)
-keep_var_index_F4 = np.where(prop_var_with_GT >= min_prop_var_with_GT)
-ds = ds[:,keep_var_index_F4]
+keep_var_index_F4 = np.where(prop_var_with_GT >= min_prop_var_with_GT)[0]
+ds = ds[keep_var_index_F4,:]
 
 # Filter 5: remove cells with genotype in < X% of variants
 prop_cell_with_GT = 1 - np.apply_along_axis(np.mean, 0, ds[:,:] == 3)
-keep_cell_index_F5 = np.where(prop_cell_with_GT >= min_prop_cell_with_GT)
-ds = ds[keep_cell_index_F5,:]
+keep_cell_index_F5 = np.where(prop_cell_with_GT >= min_prop_cell_with_GT)[0]
+ds = ds[:,keep_cell_index_F5]
 
 # Filter 6: remove variants mutated in < X% of cells
 def prop_GT_mutated(n_GT_00, n_GT_not_missing):
@@ -68,8 +71,10 @@ n_GT_00 = np.apply_along_axis(sum, 1, ds[:,:] == 0)
 n_GT_not_missing = np.apply_along_axis(sum, 1, ds[:,:] != 3)
 prop_var_mutated = prop_GT_mutated(n_GT_00, n_GT_not_missing)
 
-keep_var_index_F6 = np.where(prop_var_mutated >= min_prop_var_mutated)
-ds = ds[:,keep_var_index_F6]
+keep_var_index_F6 = np.where(prop_var_mutated >= min_prop_var_mutated)[0]
+ds = ds[keep_var_index_F6,:]
+
+# BY THIS POINT, DS is not a loom connection anymore
 
 # write output files
 
@@ -87,25 +92,20 @@ n_var = ds.shape[0]
 n_cells = ds.shape[1]
 
 for var_index in range(n_var):
-    
     chr = ds.ra["CHROM"][var_index]
     pos = ds.ra["POS"][var_index]
     ref = ds.ra["REF"][var_index]
     alt = ds.ra["ALT"][var_index]
-
     for cb_index in range(n_cells):
-        
         cb = ds.ca["barcode"][cb_index]
         gt = ds_GT_copy[var_index, cb_index]
         dp = ds["DP"][var_index, cb_index]
         ad_ref = ds["RO"][var_index, cb_index]
         ad_alt = ds["AD"][var_index, cb_index]
         gq = ds["GQ"][var_index, cb_index]
-
         var_cb_list = [str(x) for x in [chr, pos, ref, alt, cb, gt, dp, ad_ref, ad_alt, gq]]
         var_line = "\t".join(var_cb_list) + "\n"
         var_cb.write(var_line)
-
     var = ds[var_index,:]
     n_na = (var == 3).sum()
     n_not_na = n_cells - n_na
