a2c8e196548ba70931870c4c95e7619582e74d70
Steven Foltz (@envest) <stevenmasonfoltz@gmail.com>
2024-06-28 13:18:16 -0400

    Use input list of barcodes to select cells from loom file

---
+++ select_cells.py
@@ -0,0 +1,18 @@
+import sys
+import loompy
+import numpy as np
+
+loom_filename = sys.argv[1]
+bc_filename = sys.argv[2]
+selected_cells_loom_filename = sys.argv[3]
+
+# barcodes
+bc_file = open(bc_filename, "r")
+bc_list = bc_file.readlines().strip().split()
+
+# create new loom file and select cells to fill it
+with loompy.new(selected_cells_loom_filename) as selected_cells_ds:
+  with loompy.connect(loom_filename) as ds:
+    keep_barcodes_index = np.where(ds.ca["barcode"] in bc_list)[0]
+    for (ix, selection, view) in ds.scan(items = keep_barcodes, axis = 1):
+      selected_cells_ds.add_columns(view.layers, col_attrs = view.ca, row_attrs = view.ra)
