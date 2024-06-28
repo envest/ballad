1a765a8b5748e6a58308d1671a7c8a5b8ffd19e4
Steven Foltz (@envest) <stevenmasonfoltz@gmail.com>
2024-06-28 13:28:31 -0400

    Some fixes after testing

--- select_cells.py
+++ select_cells.py
@@ -13,6 +13,6 @@ bc_list = bc_file.readlines().strip().split()
 # create new loom file and select cells to fill it
 with loompy.new(selected_cells_loom_filename) as selected_cells_ds:
   with loompy.connect(loom_filename) as ds:
-    keep_barcodes_index = np.where(ds.ca["barcode"] in bc_list)[0]
-    for (ix, selection, view) in ds.scan(items = keep_barcodes, axis = 1):
+    keep_barcodes_index = np.where([x in bc_list for x in ds.ca["barcode"]])[0]
+    for (ix, selection, view) in ds.scan(items = keep_barcodes_index, axis = 1):
       selected_cells_ds.add_columns(view.layers, col_attrs = view.ca, row_attrs = view.ra)
