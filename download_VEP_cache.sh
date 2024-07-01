2907c324e19d71e7f8a86e55a3914e7f46cf83e0
envest <stevenmasonfoltz@gmail.com>
2024-07-01 10:07:34 -0400

    Update to version 112

--- download_VEP_cache.sh
+++ download_VEP_cache.sh
@@ -8,15 +8,15 @@ mkdir -p ${cache_dir}
 
 cd ${cache_dir}
 
-curl -O https://ftp.ensembl.org/pub/release-111/variation/indexed_vep_cache/homo_sapiens_vep_111_GRCh38.tar.gz
-tar xzf homo_sapiens_vep_111_GRCh38.tar.gz
-rm -f homo_sapiens_vep_111_GRCh38.tar.gz
+curl -O https://ftp.ensembl.org/pub/release-112/variation/indexed_vep_cache/homo_sapiens_vep_112_GRCh38.tar.gz
+tar xzf homo_sapiens_vep_112_GRCh38.tar.gz
+rm -f homo_sapiens_vep_112_GRCh38.tar.gz
 
-curl -O https://ftp.ensembl.org/pub/release-111/variation/indexed_vep_cache/homo_sapiens_vep_111_GRCh37.tar.gz
-tar xzf homo_sapiens_vep_111_GRCh37.tar.gz
-rm -f homo_sapiens_vep_111_GRCh37.tar.gz
+curl -O https://ftp.ensembl.org/pub/release-112/variation/indexed_vep_cache/homo_sapiens_vep_112_GRCh37.tar.gz
+tar xzf homo_sapiens_vep_112_GRCh37.tar.gz
+rm -f homo_sapiens_vep_112_GRCh37.tar.gz
 
-curl -O https://ftp.ensembl.org/pub/release-111/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz
+curl -O https://ftp.ensembl.org/pub/release-112/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz
 gzip -d Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz
 bgzip -i Homo_sapiens.GRCh38.dna.primary_assembly.fa
 
