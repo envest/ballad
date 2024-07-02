0f725160730d21ccb3c11fefca92fe2411852953
Steven Foltz (@envest) <stevenmasonfoltz@gmail.com>
2024-07-02 16:59:42 -0400

    Gather evidence from RNA and ATAC

--- filter_loom.py
+++ filter_loom.py
@@ -6,8 +6,9 @@ loom_filename = sys.argv[1]
 output_file_prefix = sys.argv[2]
 output_dir = sys.argv[3]
 
-annotation_loom_file = sys.argv[4]
-rna_loom_file = sys.argv[5]
+liftover_file = sys.argv[4]
+annotation_loom_file = sys.argv[5]
+rna_loom_file = sys.argv[6]
 #atac_loom_file = sys.argv[6]
 
 gq_min = 30
@@ -20,6 +21,17 @@ min_prop_var_mutated = 0.01
 var_cb_file = output_dir + "/" + output_file_prefix + ".variant_cell_barcode.tsv"
 var_sum_file = output_dir + "/" + output_file_prefix + ".variant_summary.tsv"
 filtered_loom_file = output_dir + "/" + output_file_prefix + ".loom"
+
+liftover_dict = {"GRCh37" : {}, "GRCh38" : {}}
+with open(liftover_file, "r") as lo:
+  for line in lo.readlines():
+    lo_list = line.strip().split()
+    if lo_list[2] != "NA" and len(lo_list) == 6:
+      hg19_chr, hg19_pos, hg38_chr, hg38_pos, REF, ALT = lo_list
+      GRCh37_variant = ":".join(["chr" + hg19_chr, str(hg19_pos), REF, ALT])
+      GRCh38_variant = ":".join(["chr" + hg38_chr, str(hg38_pos), REF, ALT])
+      liftover_dict["GRCh37"][GRCh37_variant] = GRCh38_variant
+      liftover_dict["GRCh38"][GRCh38_variant] = GRCh37_variant
 
 # set up dictionaries to modify/filter
 layers_dict = {}
@@ -35,41 +47,60 @@ with loompy.connect(loom_filename) as ds:
   for k,v in ds.ca.items():
     col_dict[k] = v
 
-row_dict["variant"] = [":".join([c, str(p), r, a]) for c,p,r,a in 
+row_dict["variant_GRCh37"] = [":".join(["chr" + str(c), str(p), r, a]) for c,p,r,a in 
   zip(row_dict["CHROM"], 
     row_dict["POS"], 
     row_dict["REF"], 
     row_dict["ALT"])]
+    
+row_dict["variant_GRCh38"] = [liftover_dict["GRCh37"][x] if x in liftover_dict["GRCh37"] else None for x in row_dict["variant_GRCh37"]]
 
 # set up annotation, RNA, and ATAC dictionaries
 with loompy.connect(annotation_loom_file) as ds_annot:
   annot_dict = {}
   for k in ds_annot.ra.keys():
     annot_dict[k] = []
-  for v in row_dict["variant"]:
-    if v in ds_annot.ra["variant"]:
-      annot_index = np.where(ds_annot.ra["variant"] == v)[0]
-    else:
-      annot_index = None
+  for v in row_dict["variant_GRCh38"]:
+    annot_index = None
+    if v is not None and v in ds_annot.ra["variant"]:
+      annot_index = np.where(ds_annot.ra["variant"] == v)[0][0]
     for k in ds_annot.ra.keys():
       if annot_index is None:
         annot_dict[k].append(None)
       else:
-        annot_dict[k].append(ds_annot[k][annot_index])
-        
+        annot_dict[k].append(ds_annot.ra[k][annot_index])
+
+# how many cells are REF/ALT from RNA
 with loompy.connect(rna_loom_file) as ds_rna:
-  rna_dict = {"n_cells_REF" : [], "n_cells_ALT" = [], "n_cells_missing" = []}
+  rna_n_cells_ref = np.zeros(ds_rna.shape[0])
+  rna_n_cells_alt = np.zeros(ds_rna.shape[0])
+  for(ix, selection, view) in ds_rna.scan(axis = 0):
+    rna_n_cells_ref[selection.min():selection.max() + 1] = np.apply_along_axis(sum, 1, view[:,:] == 1)
+    rna_n_cells_alt[selection.min():selection.max() + 1] = np.apply_along_axis(sum, 1, view[:,:] > 1)
+  rna_n_cells_missing = ds_rna.shape[1] - rna_n_cells_ref - rna_n_cells_alt
+
+# how many cells are REF/ALT from ATAC  
+with loompy.connect(atac_loom_file) as ds_atac:
+  atac_n_cells_ref = np.zeros(ds_atac.shape[0])
+  atac_n_cells_alt = np.zeros(ds_atac.shape[0])
+  for(ix, selection, view) in ds_atac.scan(axis = 0):
+    atac_n_cells_ref[selection.min():selection.max() + 1] = np.apply_along_axis(sum, 1, view[:,:] == 1)
+    atac_n_cells_alt[selection.min():selection.max() + 1] = np.apply_along_axis(sum, 1, view[:,:] > 1)
+  atac_n_cells_missing = ds_atac.shape[1] - atac_n_cells_ref - atac_n_cells_alt
+
+# gather RNA information about each variant
+with loompy.connect(rna_loom_file) as ds_rna:  
+  rna_dict = {"n_cells_REF" : np.zeros(), "n_cells_ALT" : [], "n_cells_missing" : []}
   for k in ds_rna.ra.keys():
     rna_dict[k] = []
-  for v in row_dict["variant"]:
-    if v in ds_rna.ra["variant"]:
-      rna_index = np.where(ds_rna.ra["variant"] == v)[0]
-      n_cells_REF = sum(ds_rna[rna_index,:] == 1)
-      n_cells_ALT = sum(ds_rna[rna_index,:] > 1)
-      n_cells_missing = sum(ds_rna[rna_index,:] == 0)
+  for v in row_dict["variant_GRCh38"]:
+    rna_index = None
+    if v is not None and v in ds_rna.ra["variant"]:
+      rna_index = np.where(ds_rna.ra["variant"] == v)[0][0]
+      n_cells_REF = rna_n_cells_ref[rna_index]
+      n_cells_ALT = rna_n_cells_alt[rna_index]
+      n_cells_missing = rna_n_cells_missing[rna_index]
     else:
-      print("Variant " + v + " missing from RNA vartrix")
-      rna_index = None
       n_cells_REF = -1
       n_cells_ALT = -1
       n_cells_missing = -1
@@ -80,7 +111,36 @@ with loompy.connect(rna_loom_file) as ds_rna:
       if rna_index is None:
         rna_dict[k].append(None)
       else:
-        rna_dict[k].append(ds_rna[k][rna_index])
+        rna_dict[k].append(ds_rna.ra[k][rna_index])
+        
+rna_dict["n_cells_ALT"] = np.array(rna_dict["n_cells_ALT"])
+
+# gather ATAC information about each variant
+with loompy.connect(atac_loom_file) as ds_atac:  
+  atac_dict = {"n_cells_REF" : np.zeros(), "n_cells_ALT" : [], "n_cells_missing" : []}
+  for k in ds_atac.ra.keys():
+    atac_dict[k] = []
+  for v in row_dict["variant_GRCh38"]:
+    atac_index = None
+    if v is not None and v in ds_atac.ra["variant"]:
+      atac_index = np.where(ds_atac.ra["variant"] == v)[0][0]
+      n_cells_REF = atac_n_cells_ref[atac_index]
+      n_cells_ALT = atac_n_cells_alt[atac_index]
+      n_cells_missing = atac_n_cells_missing[atac_index]
+    else:
+      n_cells_REF = -1
+      n_cells_ALT = -1
+      n_cells_missing = -1
+    atac_dict["n_cells_REF"].append(n_cells_REF)
+    atac_dict["n_cells_ALT"].append(n_cells_ALT)
+    atac_dict["n_cells_missing"].append(n_cells_missing)
+    for k in ds_atac.ra.keys():
+      if atac_index is None:
+        atac_dict[k].append(None)
+      else:
+        atac_dict[k].append(ds_atac.ra[k][atac_index])
+        
+atac_dict["n_cells_ALT"] = np.array(atac_dict["n_cells_ALT"])
 
 # Filters 1-3: set genotype to missing if GQ < gq_min, DP < dp_min, or scVAF < scVAF_min
 # can be applied simultaneously because each genotype is independent
@@ -140,7 +200,8 @@ n_GT_00 = np.apply_along_axis(sum, 1, layers_dict[""] == 0)
 n_GT_not_missing = np.apply_along_axis(sum, 1, layers_dict[""] != 3)
 prop_var_mutated = prop_GT_mutated(n_GT_00, n_GT_not_missing)
 
-keep_var_index_F6 = np.where(prop_var_mutated >= min_prop_var_mutated or rna_dict["n_cells_ALT"] > 0)[0]
+multiome_evidence = np.logical_or(rna_dict["n_cells_ALT"] > 0, atac_dict["n_cells_ALT"] > 0)
+keep_var_index_F6 = np.where(prop_var_mutated >= min_prop_var_mutated or multiome_evidence)[0]
 
 for k,v in layers_dict.items():
   layers_dict[k] = v[keep_var_index_F6,:]
