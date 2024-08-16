6f130f53058696c5128becbb427fee30770fd993
envest <stevenmasonfoltz@gmail.com>
2024-08-16 15:16:15 -0400

    Adjust p-values only where there are mutations; print out mutation info for each barcode

--- mutations_by_cell_type.py
+++ mutations_by_cell_type.py
@@ -14,15 +14,19 @@ cell_barcodes_filename = sys.argv[3]
 output_dir = sys.argv[4]
 output_prefix = sys.argv[5]
 
-output_filename = os.path.join(output_dir, output_prefix + ".mutations_by_cell_type.tsv")
+output_mutations_by_cell_type_filename = os.path.join(output_dir, output_prefix + ".mutations_by_cell_type.tsv")
+output_mutations_by_barcode_filename = os.path.join(output_dir, output_prefix + ".mutations_by_barcode.tsv")
 
 if not os.path.isdir(output_dir):
   sys.exit(output_dir + " directory does not exist")
 
 cell_type_dict = {}
+barcode_dict = {}
 with open(cell_barcodes_filename, "r") as f:
     for line in f.readlines():
         bc, tumor_normal, level_one, level_two, umap1, umap2 = line.strip().split()
+
+        barcode_dict[bc] = {"L0" : tumor_normal, "L1" : level_one, "L2" : level_two, "UMAP1" : umap1, "UMAP2" : umap2, "mutation" : {}}
 
         compartment_0 = ":".join([str(x) for x in ["L0", tumor_normal, "NA", "NA"]])
         compartment_1 = ":".join([str(x) for x in ["L1", tumor_normal, level_one, "NA"]])
@@ -52,7 +56,10 @@ with open(mutations_filename, "r") as f:
             n_00 = sum(ds[m_index,] == 0)
             n_01 = sum(ds[m_index,] == 1)
             n_11 = sum(ds[m_index,] == 2)
-            mutation_prop[m] = (n_01 + n_11)/(n_00 + n_01 + n_11)
+            if n_00 + n_01 + n_11 > 0:
+                mutation_prop[m] = (n_01 + n_11)/(n_00 + n_01 + n_11)
+            else:
+                mutation_prop[m] = 0
         else:
             print("Mutation " + m + " not in loom file")
   
@@ -68,6 +75,18 @@ for m,i in mutation_dict.items():
     cells_11 = ds.ca["barcode"][np.where(ds[i,] == 2)]
     cells_na = ds.ca["barcode"][np.where(ds[i,] == 3)]
   
+    for bc in ds.ca["barcode"]:
+
+        gt = "NA"
+        if bc in cells_00:
+            gt = 0
+        elif bc in cells_01:
+            gt = 1
+        elif bc in cells_11:
+            gt = 2
+        
+        barcode_dict[bc]["mutation"][m] = gt
+
     for compartment in cell_type_dict.keys():
 
         n_00 = sum([x in cells_00 for x in cell_type_dict[compartment]])
@@ -78,27 +97,68 @@ for m,i in mutation_dict.items():
         cell_type_n = n_00 + n_01 + n_11 + n_na
         cell_type_cov = n_00 + n_01 + n_11
         cell_type_mut = n_01 + n_11
+
+        if cell_type_cov > 0:
+            cell_type_prop = cell_type_mut/cell_type_cov
+            # using overall proportion of mutated cells (misleading if sample dominated by cell type with most of mutations)
+            binom_result = binomtest(k = cell_type_mut, n = cell_type_cov, p = mutation_prop[m], alternative = "greater")
+            # using sequencing by synthesis error rate of 0.25% (0.0025)
+            # H0: the mutations observed in this cell type are not greater than would be expected by random error
+            if cell_type_mut > 0:
+                binom_result = binomtest(k = cell_type_mut, n = cell_type_cov, p = 0.0025, alternative = "greater")
+                binom_p = binom_result.pvalue
+            else:
+                binom_p = "NA"
+        else:
+            cell_type_prop = "NA"
+            binom_p = "NA"
     
-        binom_result = binomtest(k = cell_type_mut, n = cell_type_cov, p = mutation_prop[m], alternative = "greater")
-        binom_p = binom_result.pvalue
-  
-        output_line_list = [m] + compartment.split(":") + [x for x in [cell_type_n, cell_type_cov, n_00, n_01, n_11, n_na, cell_type_mut, cell_type_mut/cell_type_cov, mutation_prop[m], binom_p]]
+        output_line_list = [m] + compartment.split(":") + [x for x in [cell_type_n, cell_type_cov, n_00, n_01, n_11, n_na, cell_type_mut, cell_type_prop, mutation_prop[m], binom_p]]
 
         for k,v in zip(output_header_list, output_line_list):
             output_list_dict[k].append(v)
 
 # BH adjusted p-value
-adjusted_pvalues = false_discovery_control(output_list_dict["binomial_pvalue"], method = "bh")
+fdr_pvalues_input_list = []
+fdr_pvalues_output_list = []
+for p in output_list_dict["binomial_pvalue"]:
+    if p != "NA":
+        fdr_pvalues_input_list.append(p)
+
+if fdr_pvalues_input_list != []:
+    fdr_pvalues_output_list = false_discovery_control(fdr_pvalues_input_list, method = "bh")
+
+adjusted_index = 0
+adjusted_pvalues = []
+for p in output_list_dict["binomial_pvalue"]:
+    if p != "NA":
+        adjusted_pvalues.append(fdr_pvalues_output_list[adjusted_index])
+        adjusted_index += 1
+    else:
+        adjusted_pvalues.append("NA")
+
 output_list_dict["adjusted_pvalue"] = adjusted_pvalues
 
-output_file = open(output_filename, "w")
-output_file.write("\t".join(output_header_list + ["adjusted_pvalue"]) + "\n")
+output_mutations_by_cell_type_file = open(output_mutations_by_cell_type_filename, "w")
+output_mutations_by_cell_type_file.write("\t".join(output_header_list + ["adjusted_pvalue"]) + "\n")
 
 n_lines = len(adjusted_pvalues)
 for i in range(n_lines):
     output_line_list = [str(output_list_dict[x][i]) for x in output_header_list + ["adjusted_pvalue"]]
-    output_file.write("\t".join(output_line_list) + "\n")
+    output_mutations_by_cell_type_file.write("\t".join(output_line_list) + "\n")
+
+output_barcode_file = open(output_mutations_by_barcode_filename, "w")
+output_barcode_header_list = ["barcode", "level_zero", "level_one", "level_two", "UMAP1", "UMAP2"] + list(mutation_dict.keys())
+output_barcode_file.write("\t".join(output_barcode_header_list) + "\n")
+
+for bc in ds.ca["barcode"]:
+
+    barcode_list = [bc, barcode_dict[bc]["L0"], barcode_dict[bc]["L1"], barcode_dict[bc]["L2"], barcode_dict[bc]["UMAP1"], barcode_dict[bc]["UMAP2"]] + \
+            [barcode_dict[bc]["mutation"][m] for m in mutation_dict.keys()]
+    output_barcode_file.write("\t".join([str(x) for x in barcode_list]) + "\n")
+
 
 ds.close()
-output_file.close()
+output_mutations_by_cell_type_file.close()
+output_barcode_file.close()
 
